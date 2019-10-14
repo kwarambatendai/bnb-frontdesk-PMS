f93e5729b6d13a80c0f34d72004d39bdda48bb34
kwarambatendai <kwarambatendai@gmail.com>
2019-10-14 09:43:06 +0200

    more linting

--- hotel booking/admin/include/check_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-// PHP Hotel Booking 
-// Copyright (c) All Rights Reserved, NetArt Media 2003-2017
-// Check http://www.netartmedia.net/php-hotel-booking for demos and information
-// Released under the MIT license
-?>
