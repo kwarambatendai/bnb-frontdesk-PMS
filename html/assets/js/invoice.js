af12167e9135f296c9b4b28e481b4f14720e5644
kwarambatendai <kwarambatendai@gmail.com>
2019-10-16 20:06:56 +0200

    this is fooling around

---
+++ html/assets/js/invoice.js
@@ -0,0 +1,14 @@
+jQuery().invoice({
+            addRow: "#addRow",
+            delete: ".delete",
+            parentClass: ".item-row",
+
+            price: ".rate",
+            qty: ".days",
+            total: ".total",
+            totalQty: "#totalQty",
+
+            subtotal: "#subtotal",
+            discount: "#discount",
+            shipping: "#stourismLevy",
+            grandTotal: "#grandTotal"
