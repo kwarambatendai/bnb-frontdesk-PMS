9ee235d86b0a2049e39fd6d544e4fd5c728c41a9
kwarambatendai <kwarambatendai@gmail.com>
2019-10-18 12:20:07 +0200

    the invoice man

--- html/assets/js/pages/tables/invoice.js
+++ html/assets/js/pages/tables/invoice.js
@@ -1,46 +1,45 @@
-$(function () {
+$(function() {
     $('.js-basic-example').DataTable();
 });
 
 /* Formatting function for row details - modify as you need */
-function format ( d ) {
+function format(d) {
     // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Full name:</td>'+
-            '<td>'+d.name+'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<td>Extension number:</td>'+
-            '<td>'+d.extn+'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<td>Extra info:</td>'+
-            '<td>And any further details here (images etc)...</td>'+
-        '</tr>'+
-    '</table>';
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '<td>Full name:</td>' +
+        '<td>' + d.name + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Extension number:</td>' +
+        '<td>' + d.extn + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Extra info:</td>' +
+        '<td>And any further details here (images etc)...</td>' +
+        '</tr>' +
+        '</table>';
 }
- 
+
 $(document).ready(function() {
-    
-     
+
+
     // Add event listener for opening and closing details
-    $('#example tbody').on('click', 'td.details-control', function () {
+    $('#example tbody').on('click', 'td.details-control', function() {
         var tr = $(this).closest('tr');
-        var row = table.row( tr );
- 
-        if ( row.child.isShown() ) {
+        var row = table.row(tr);
+
+        if (row.child.isShown()) {
             // This row is already open - close it
             row.child.hide();
             tr.removeClass('shown');
-        }
-        else {
+        } else {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child(format(row.data())).show();
             tr.addClass('shown');
         }
-    } );
-} );
+    });
+});
 
 // Add row into table
 var addRowTable = {
@@ -81,7 +80,7 @@ var addRowTable = {
                 confirmButtonColor: "#dc3545",
                 confirmButtonText: "Yes, delete it!",
                 closeOnConfirm: false
-            }, function () {
+            }, function() {
                 object.rowRemove($row)
                 swal("Deleted!", "Your imaginary file has been deleted.", "success");
             });
